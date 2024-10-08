b2458b91afb968888b8ef20ad4df2663d30abd78
Josue Cano <jdevelopcano@gmail.com>
2024-10-08 12:53:47 -0600

    added intro js

---
+++ 07-intro-js/app.js
@@ -0,0 +1,43 @@
+// alert("Hola mundo");
+/*
+
+un comentario multilinea
+otro comentario
+*/
+//const no permite cambiar el valor de la variable
+//string o sea tipo texto
+const name = "Josue";
+const name1 =  "Jorge";
+const name2 =  "Muriel";
+const name3 =  "Luis";
+//number todos los tipos de numero desde enteros, negativos o con decimales
+let numero = 12;
+let suma  = 12.5;
+let infinito = Infinity;
+let menosInfinito = -Infinity;
+let noEsUnNumero = NaN;
+//boolean
+let esVerdad = true;
+let esFalso = false;
+//tipos vacios
+let anime = undefined; // no esta definido el contenido
+let animal = null; // no hay contenido
+
+//
+//let permite cambiar el valor de la variable
+let apellido = "Cano";
+
+// alert("Hola flor desde el alert");
+console.log("Hola " + name);
+console.log("Hola " + name1);
+console.log("Hola " + name2);
+console.log("Hola " + name3);
+// name = "Martha";
+console.log("Hola " + apellido);
+apellido = "Villalobos";
+console.log("Hola " + apellido);
+
+console.log(1 + "11");
+console.log("11" - 1);
+console.log(4 * "7");
+// console.error("Hola cesar");
---
+++ 07-intro-js/prompt.js
@@ -0,0 +1,14 @@
+// const favoriteAnimal = prompt("Ingresa tu animal favorito");
+// console.log(favoriteAnimal);
+let age = +prompt("Ingresa tu edad");
+console.log(typeof age);
+// console.log("Tu edad dentro de 5 años es " + (Number(age) + 5));
+
+/* 
+  1.- Preguntarle al usuario su nombre con apellido
+  2.- Preguntarle su comida favorita
+  3.- Su edad 
+  4.- Sumarle 10 años a la edad actual 
+  5.- Utilizando console.log  mostrar un texto elaborado 
+  con la data obtenida en el prompt
+*/
